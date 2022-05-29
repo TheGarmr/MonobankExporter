d0f9b3af17c8250231ba587a986069c9b3f028ae
Garmr <garmr@honchar.net>
2022-05-29 21:15:01 +0300

    Cleanup of usings at Startup

--- MonobankExporter.API/Startup.cs
+++ MonobankExporter.API/Startup.cs
@@ -1,9 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using MonobankExporter.API.Extensions;
 using MonobankExporter.API.Middleware;
 using Prometheus;
